625c6d5816728c5e8f9a417487d78351488f7123
Nikhilkowluru7 <nikhilkowluru@gmail.com>
2025-10-26 09:52:59 +0000

    Alphabet HW

---
+++ Alphabet HW.py
@@ -0,0 +1,5 @@
+character = input("Enter only 1 character from your keyboard: ")
+if character=="a" or "b" or "c" or "d" or "e" or"f"or"g"or"h"or "i" or "j" or "k" or "l" or "m" or "n" or "o" or "p" or "q" or "r" or "s" or "t" or "u" or "v" or "w" or "x" or "y" or "z" :
+    print("Your character is from the alphabet!")
+else:
+    print("Your character is not from the alphabet!")
